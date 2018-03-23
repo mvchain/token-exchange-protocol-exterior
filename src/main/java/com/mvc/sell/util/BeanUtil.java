43126bd4a9b1ae4544898ead5bde111053cf2509
戚羿辰 <416350144@qq.com>
2018-03-23 16:00:02 +0800

    update bean util

--- src/main/java/com/mvc/sell/util/BeanUtil.java
+++ src/main/java/com/mvc/sell/util/BeanUtil.java
@@ -1,9 +1,16 @@
 package com.mvc.sell.util;
 
 import com.github.pagehelper.PageInfo;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.springframework.beans.BeanUtils;
+import org.springframework.cglib.beans.BeanMap;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -14,8 +21,8 @@ import java.util.stream.Collectors;
  */
 public class BeanUtil {
 
-    public static PageInfo beanList2VOList (List<? extends Object> list , Class targetClass) {
-        List<Object> retult = list.stream().map(account1 -> {
+    public static PageInfo beanList2VOList(PageInfo<? extends  Object> obj, Class targetClass) {
+        List retult = obj.getList().stream().map(account1 -> {
             Object instance = null;
             try {
                 instance = targetClass.newInstance();
@@ -27,7 +34,8 @@ public class BeanUtil {
             }
             return instance;
         }).collect(Collectors.toList());
-        return new PageInfo(retult);
+        obj.setList(retult);
+        return obj;
     }
 
     public static Object copyProperties(Object source, Object target) {
@@ -35,5 +43,65 @@ public class BeanUtil {
             BeanUtils.copyProperties(source, target);
         }
         return target;
+    }
+
+    public static <T> Map<String, Object> beanToMap(T bean) {
+        Map<String, Object> map = Maps.newHashMap();
+        if (bean != null) {
+            BeanMap beanMap = BeanMap.create(bean);
+            for (Object key : beanMap.keySet()) {
+                map.put(key+"", beanMap.get(key));
+            }
+        }
+        return map;
+    }
+
+    public static <T> T mapToBean(Map<String, Object> map,T bean) {
+        BeanMap beanMap = BeanMap.create(bean);
+        beanMap.putAll(map);
+        return bean;
+    }
+
+    public static <T> List<Map<String, Object>> objectsToMaps(List<T> objList) {
+        List<Map<String, Object>> list = Lists.newArrayList();
+        if (objList != null && objList.size() > 0) {
+            Map<String, Object> map = null;
+            T bean = null;
+            for (int i = 0,size = objList.size(); i < size; i++) {
+                bean = objList.get(i);
+                map = beanToMap(bean);
+                list.add(map);
+            }
+        }
+        return list;
+    }
+
+    public static <T> List<T> mapsToObjects(List<Map<String, Object>> maps,Class<T> clazz) throws InstantiationException, IllegalAccessException {
+        List<T> list = Lists.newArrayList();
+        if (maps != null && maps.size() > 0) {
+            Map<String, Object> map = null;
+            T bean = null;
+            for (int i = 0,size = maps.size(); i < size; i++) {
+                map = maps.get(i);
+                bean = clazz.newInstance();
+                mapToBean(map, bean);
+                list.add(bean);
+            }
+        }
+        return list;
+    }
+
+    public static MultiValueMap<String, String> beanToStringMap(Object bean) {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap();
+        if (bean != null) {
+            BeanMap beanMap = BeanMap.create(bean);
+            for (Object key : beanMap.keySet()) {
+                Object value = beanMap.get(key);
+                if (null != beanMap.get(key)) {
+                    map.put(key+"", Arrays.asList(String.valueOf(value)));
+                }
+            }
+        }
+        return map;
     }
 }
