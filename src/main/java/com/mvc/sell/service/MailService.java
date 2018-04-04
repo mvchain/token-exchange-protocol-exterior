a127d1ba71f4c5315b23b756e9123801c4a7c376
戚羿辰 <416350144@qq.com>
2018-04-04 12:52:32 +0800

    fix bug

--- src/main/java/com/mvc/sell/service/MailService.java
+++ src/main/java/com/mvc/sell/service/MailService.java
@@ -3,6 +3,7 @@ package com.mvc.sell.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,12 +18,13 @@ public class MailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void send(String email, String code) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(email);
         message.setFrom("buytoken@163.com");
-        message.setSubject(String.format("您的验证码是 %s , 请在5分钟内输入", code));
-        message.setText(String.format("buyToken 验证码: %s", code));
+        message.setSubject(String.format("buytoken verification code"));
+        message.setText(String.format("your Verification code is %s , please valid within 5 minutes", code));
         mailSender.send(message);
     }
 }
